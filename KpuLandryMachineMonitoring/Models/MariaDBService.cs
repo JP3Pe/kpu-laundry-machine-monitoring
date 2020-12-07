82687ff451dc27df9da305df1c4854450eac8a42
JP3Pe <rlaworhkd430430@gmail.com>
2020-12-07 11:16:53 +0900

    up machine_id 반환값을 기반으로 세탁기와 건조기 이미지 판별

--- KpuLandryMachineMonitoring/Models/MariaDBService.cs
+++ KpuLandryMachineMonitoring/Models/MariaDBService.cs
@@ -22,14 +22,14 @@ namespace KpuLandryMachineMonitoring.Models
         {
             var studentUseStateResultList = new List<StudentUseState>();
             const string sql = @"
-                SELECT name, room_number, phone_number, start_time, end_time
+                SELECT name, room_number, phone_number, machine_id, start_time, end_time
                 FROM student
-                    INNER JOIN use_state us on student.student_number = us.student_number
-                    WHERE machine_id in (
-                        SELECT machine_id
-                        FROM laundry_machine
-                        WHERE locate_floor = 3
-                    )
+                         INNER JOIN use_state us on student.student_number = us.student_number
+                WHERE machine_id in (
+                    SELECT machine_id
+                    FROM laundry_machine
+                    WHERE locate_floor = 3
+                )
                 ORDER BY machine_id ASC;
             ";
 
@@ -44,36 +44,12 @@ namespace KpuLandryMachineMonitoring.Models
                     Name = reader["name"].ToString(),
                     RoomNumber = reader["room_number"].ToString(),
                     PhoneNumber = reader["phone_number"].ToString(),
+                    MachineID = Convert.ToInt32(reader["machine_id"]),
                     StartTime = Convert.ToDateTime(reader["start_time"]),
                     EndTime = Convert.ToDateTime(reader["end_time"])
                 });
 
             return studentUseStateResultList;
-        }
-
-        // TODO: machine_id가 건조기인지 판별하기
-        public bool IsDryerMachine(int dryerMachineId)
-        {
-            int[] dryerListResult;
-            const string sql = @"SELECT machine_id FROM laundry_machine WHERE machine_type = 'd';";
-            var dryerList = new List<LandryMachine>();
-
-            using var connection = GetConnection();
-            connection.Open();
-
-            var command = new MySqlCommand(sql, connection);
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
-                dryerList.Add(new LandryMachine
-                {
-                    MachineID = Convert.ToInt32(reader["machine_id"])
-                });
-
-            foreach (var landryMachine in dryerList)
-                if (landryMachine.MachineID == dryerMachineId)
-                    return true;
-
-            return false;
         }
     }
 }
