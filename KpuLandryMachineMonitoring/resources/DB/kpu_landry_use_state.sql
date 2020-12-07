63769ce8e7254f7eb55b35ef8f55baf5f8fff388
JP3Pe <rlaworhkd430430@gmail.com>
2020-12-07 22:12:00 +0900

    up 잘못된 use_state 테이블의 PK 삭제

--- KpuLandryMachineMonitoring/resources/DB/kpu_landry_use_state.sql
+++ KpuLandryMachineMonitoring/resources/DB/kpu_landry_use_state.sql
@@ -4,13 +4,12 @@ create table use_state
     end_time       datetime not null,
     student_number int      not null,
     machine_id     int      not null,
-    primary key (machine_id, student_number),
     constraint use_state_laundry_machine_machine_id_fk
         foreign key (machine_id) references laundry_machine (machine_id),
     constraint use_state_student_student_number_fk
         foreign key (student_number) references student (student_number)
 )
-    comment '유저의 세탁 기기 사용 현황을 저장하고 있는 테이블';
+    comment '유저의 세탁 기기 사용 현황을 저장 하고 있는 테이블';
 
 INSERT INTO kpu_landry.use_state (start_time, end_time, student_number, machine_id)
 VALUES ('2020-11-29 14:54:08', '2020-11-29 15:42:15', 2019551008, 1);
